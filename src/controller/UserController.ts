030956fce7e748982a9036b867c591bf57735b60
joaofilippe <joaofilippe@outlook.com>
2022-01-28 11:25:58 -0300

    Adicionando User Entitie

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -7,22 +7,8 @@ import Authenticator from '../services/Authenticator';
 export default class UserController {
     signup = async (req: Request, res: Response) => {
         try {
-            let message = 'Sucesso!'
             const {email, name, password} = req.body
-            if (!name || !email || !password) {
-                res.statusCode = 406
-                message = `As propriedades "name", "email" ou "password" não foram repassadas`
-                throw new Error(message)
-            }
-
-            const id: string = generateId()
-            const cypherPassword = await HashManager.hash(password)
-
-            await connection('users')
-                  .insert ({
-                      id, name, email, cypherPassword
-                  })
-            const token : string = Authenticator.generateToken(id)
+            
 
             res.status(201).send({message, token})
 

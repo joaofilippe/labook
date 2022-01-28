126bf2ada85f3625aec860a641b4b18fd1a56026
joaofilippe <joaofilippe@outlook.com>
2022-01-28 12:49:54 -0300

    updating UserBusiness

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -5,27 +5,34 @@ import HashManager from '../services/HashManager';
 import generateId from '../services/idGenerator';
 import UserDatabase from '../database/UserDatabase';
 export default class UserBusiness {
-    signup = async (input: SignupInputDTO) => {
-        let message = 'Sucesso!';
-        const { name, email, password } = input;
+    signup = async (input: SignupInputDTO): Promise<string> => {
+        try {
+            let message = 'Sucesso!';
+            const { name, email, password } = input;
 
-        if (!name || !email || !password) {
-            throw new Error('As propriedades "name", "email" ou "password" não foram repassadas');
+            if (!name || !email || !password) {
+                throw new Error(
+                    'As propriedades "name", "email" ou "password" não foram repassadas'
+                );
+            }
+
+            const id: string = generateId();
+
+            const cypherPassword = await HashManager.hash(password);
+
+            const user: User = {
+                id,
+                name,
+                email,
+                password: cypherPassword,
+            };
+
+            const userDatabase = new UserDatabase();
+            await userDatabase.insertUser(user);
+            const token: string = Authenticator.generateToken({ id });
+            return token;
+        } catch (error: any) {
+            throw new Error(error.message);
         }
-
-        const id: string = generateId();
-        
-        const cypherPassword = await HashManager.hash(password);
-
-        const user: User = {
-            id, 
-            name,
-            email,
-            password: cypherPassword
-        }
-
-        const userDatabase = new UserDatabase()
-
-        const token: string = Authenticator.generateToken({ id });
     };
 }

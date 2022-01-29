7402cfaecbf60005b798cb8bca93fd1e84e2db94
joaofilippe <joaofilippe@outlook.com>
2022-01-29 09:32:58 -0300

    Inserindo o endpoint de criar posts

--- src/database/PostDatabase.ts
+++ src/database/PostDatabase.ts
@@ -1,14 +1,18 @@
 import { Post } from '../entities/Posts';
 import BaseDatabase from './Basedatabase';
+import moment from 'moment';
 
 export default class PostDatabase extends BaseDatabase {
     create = async (input: Post) => {
-        const { id, photo, description, type, authorId } = input;
+        const { id, photo, description, type, createdAt, authorId } = input;
+        const created_at = moment(createdAt).format("YYYY-MM-DD hh:mm:ss")
+        console.log(created_at)
         const post = {
             id,
             photo,
             description,
             type,
+            created_at,
             author_id: authorId,
         };
         try {
